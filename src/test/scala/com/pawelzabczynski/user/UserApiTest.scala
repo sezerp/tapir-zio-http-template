f095976010d8fc0ce0cd52705c8a0a6a322d1b48
Pawel Zabczynski <zabczynskipawe@gmail.com>
2023-03-21 22:16:00 +0100

    tests(TZHT-T-5): create test for login endpoint

--- src/test/scala/com/pawelzabczynski/user/UserApiTest.scala
+++ src/test/scala/com/pawelzabczynski/user/UserApiTest.scala
@@ -1,17 +1,26 @@
 package com.pawelzabczynski.user
 
-import com.pawelzabczynski.test.{TestBase, TestSupport}
+import com.pawelzabczynski.test.{CreateUserResult, TestBase, TestSupport}
 import org.scalatest.concurrent.Eventually
 import sttp.model.StatusCode
 import com.pawelzabczynski.infrastructure.JsonSupport._
 
-class UserApiTest extends TestBase with Eventually with TestSupport {
+class UserApiTest extends TestBase with TestSupport with Eventually {
 
-  "[GET] register endpoint" should "return apiKey" in {
+  "[POST] register endpoint" should "return apiKey" in {
     val request  = UserRegisterRequest("account name", "user_login", "test@email.com", "some_password_123")
     val response = requests.userRegister(request)
 
     response.code shouldBe StatusCode.Ok
     response.body.shouldDeserializeTo[UserRegisterResponse]
+  }
+
+  "[POST] login endpoint" should "successfully process form and return api key" in {
+    val CreateUserResult(_, login, password, _) = requests.createUser()
+    val request                                 = UserLoginRequest(login, password)
+    val response                                = requests.userLogin(request)
+
+    response.code shouldBe StatusCode.Ok
+    response.body.shouldDeserializeTo[UserLoginResponse]
   }
 }
